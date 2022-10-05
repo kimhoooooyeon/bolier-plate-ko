77532dc4979b8deda64663b655dd82a1b1d25226
Hoyeon Kim <kimhoyeon8@gmail.com>
2022-10-05 17:16:51 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const { User } = require("./models/User")
+const bodyParser = require('body-parser')
+
+const config = require("./config/key")
+
+//application/x-www-form-
+app.use(bodyParser.urlencoded({extended : true}));
+//application
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kimhoyeon:gmIe5RUhxwJbwl17@bolierplate.gyk6iya.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
-.catch(Error => console.log(err))
+.catch(err => console.log(err))
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕핫요 !!!')
+  res.send('Hello World! 안녕하세요 !!!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/register', (req, res) => {
+  //회원가입할때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, uesrInfo) =>{
+      if(err) return res.json({ success : false, err})
+      return res.status(200).json({
+        success: true
+      })
+    })
+
 })
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`) )
